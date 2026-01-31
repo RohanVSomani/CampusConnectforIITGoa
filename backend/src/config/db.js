3e023e0942943876b962a7506a6255b536c06dab
Rohanvs <rohanvsomani@gmail.com>
2026-01-31 12:30:09 +0530

    fixed mongo

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 //connnectiing to db - do it on 26th
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/campusflow';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export async function connectDB() {
   await mongoose.connect(MONGODB_URI);
